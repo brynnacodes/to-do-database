5bd13f00a5f2b803d32c562c7006aa2f60351a82
Brynna McCarter and Michaela Davis <brynna@>
2017-02-21 11:48:37 -0800

    add date property to Task

--- src/Task.php
+++ src/Task.php
@@ -4,12 +4,14 @@
         private $description;
         private $id;
         private $category_id;
+        private $date;
 
-        function __construct($description, $id = null, $category_id)
+        function __construct($description, $id = null, $category_id, $date)
         {
             $this->description = $description;
             $this->id = $id;
             $this->category_id = $category_id;
+            $this->date = $date;
         }
 
         function setDescription($new_description)
@@ -32,9 +34,19 @@
             return $this->category_id;
         }
 
+        function setDate($new_date)
+        {
+            $this->date = (string) $new_date;
+        }
+
+        function getDate()
+        {
+            return $this->date;
+        }
+
         function save()
         {
-            $GLOBALS['DB']->exec("INSERT INTO tasks (description, category_id) VALUES ('{$this->getDescription()}', {$this->getCategoryId()})");
+            $GLOBALS['DB']->exec("INSERT INTO tasks (description, category_id, date) VALUES ('{$this->getDescription()}', {$this->getCategoryId()}, '{$this->getDate()}')");
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
@@ -46,7 +58,8 @@
                 $description = $task['description'];
                 $id = $task['id'];
                 $category_id = $task['category_id'];
-                $new_task = new Task($description, $id, $category_id);
+                $id = $date['date'];
+                $new_task = new Task($description, $id, $category_id, $date);
                 array_push($tasks, $new_task);
             }
             return $tasks;
